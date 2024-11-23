d3e1a925eb053e3a94d1de6ced4e9d8ff54a7e9c
= <lukasilverio94@gmail.com>
2024-11-23 23:10:31 +0100

    set roles middleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,20 +1,31 @@
 const express = require("express");
-
+const verifyToken = require("../middlewares/authMiddleware");
+const authorizeRoles = require("../middlewares/roleMiddleware");
 const router = express.Router();
 
 // only admin can access this route
-router.get("/admin", (req, res) => {
+router.get("/admin", verifyToken, authorizeRoles("admin"), (req, res) => {
   res.json({ message: "Welcome to Admin Dashboard" });
 });
 
 // only manager can access this route
-router.get("/manager", (req, res) => {
-  res.json({ message: "Welcome to Manager Page" });
-});
+router.get(
+  "/manager",
+  verifyToken,
+  authorizeRoles("admin", "manager"),
+  (req, res) => {
+    res.json({ message: "Welcome to Manager Page" });
+  }
+);
 
 // All can access this route
-router.get("/user", (req, res) => {
-  res.json({ message: "Welcome to User Dashboard" });
-});
+router.get(
+  "/user",
+  verifyToken,
+  authorizeRoles("admin", "manager", "user"),
+  (req, res) => {
+    res.json({ message: "Welcome to User Dashboard" });
+  }
+);
 
 module.exports = router;
